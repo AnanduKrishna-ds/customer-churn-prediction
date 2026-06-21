5336ad8a00b63c091e581894b988207f02f86817
AnanduKrishna-ds <anandukrishna427@gmail.com>
2026-06-21 14:39:50 +0530

    setup and requirement

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages,setup
+
+
+hyphen_e_dot="-e ."
+
+def get_requirements(file_path:str)->list[str]:
+    '''
+        this function return a list of requirements 
+    '''
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
+    return requirements
+
+
+
+setup(
+    name="Customer chrun ml",
+    version="0.0.1",
+    author="Anandu Krishna K S",
+    author_email="anandukrishna427@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
